811e9613b8f9b890e7e8e11841966ebd2f8c1ab8
AR <raghav.anjali@gmail.com>
2020-11-18 13:31:18 +1300

    1. added readme file 2. compeleted the python file with the required screenshots.

---
+++ README.md
@@ -0,0 +1,12 @@
+### Execution
+Execute check-packages to see if you have the required packages using check-package.py
+
+The main file - open-browser.py 
+1. opens various links as mentioned in the header of the file
+2. looks for the html feature
+3. take the screenshot
+4. save with the name as mentioned.
+
+### To run a python file
+
+```python3 <file-name>```
--- open-browser.py
+++ open-browser.py
@@ -8,7 +8,13 @@ screenshotURLs = [
     ["https://github.com/Theraghav", "github",
         "//div[@class='js-yearly-contributions']"],
     ["https://forum.uipath.com/u/TheRaga/summary", "uipath-forum",
-     "//div[@id='main-outlet']"]
+     "//div[@id='main-outlet']"],
+    ["https://stackoverflow.com/users/4581293/raga?tab=topactivity", "stackoverflow",
+     "//div[@id='top-cards']"],
+    ["https://university.mongodb.com/certified_professional_finder/certified_professionals/828741", "mongo-dev-profile",
+     "//div[@class='css-i9gxme e1p474gp5']"],
+    ["https://www.certmetrics.com/uipath/public/badge.aspx?i=1&t=c&d=2020-11-09&ci=UIP00133845", "uiPath-cert",
+        "//div[@id='prevDivPublic']"],
 ]
 
 
@@ -27,13 +33,17 @@ def main():
         print(urlInfo)
         try:
             url = urlInfo[0]  # "https://github.com/Theraghav"
-            name = urlInfo[1]  # "github"
-            # "//div[@class='js-yearly-contributions']"
+            name = urlInfo[1]  # "github" # "//div[@class='js-yearly-contributions']"
             screenshot_element = urlInfo[2]
             screenshot_filename = "output/"+name+"-screenshot.png"
             open_the_website(url)
-            time.sleep(2) # allow the page to load fully
+            time.sleep(2)  # allow the page to load fully
             store_screenshot(screenshot_element, screenshot_filename)
+        except Exception as inst:
+            print(type(inst))    # the exception instance
+            print(inst.args)     # arguments stored in .args
+            print(inst)
+            print('Could not get screenshot for '+name)
         finally:
             browser.close_all_browsers()
 
